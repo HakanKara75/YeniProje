f220fac979e7173649abf008fa510624c8baa209
HakanKara75 <hakanbatirhan@gmail.com>
2023-03-06 23:01:59 +0300

    v

--- src/Ab01.java
+++ src/Ab01.java
@@ -1,7 +1,35 @@
+import connect_db.DbHelper;
+
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.time.LocalDate;
 
 public  class Ab01 {
-            public static void main(String[] args) {
 
-    }
+
+
+            public static void main(String[] args) throws SQLException {
+                Connection connection= null;
+                DbHelper helper=new DbHelper();
+                Statement statement=null;
+                ResultSet resultSet;
+                try{
+
+                    connection= helper.getConnection();
+                    statement=connection.prepareStatement(
+                            "insert into sakila.actor(first_name) values (Tarik)");
+
+
+                    resultSet=statement.executeQuery("SELECT first_name from sakila.actor");
+                    while (resultSet.next());
+                    System.out.println(resultSet.getString("first_name"));
+
+
+                }catch(SQLException e){
+                   helper.showErrorMessage(e);
+                } finally  {
+                   connection.close();
+                }
+
+            }
 }
---
+++ src/connect_db/DbHelper.java
@@ -0,0 +1,24 @@
+package connect_db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbHelper {
+
+
+    static  String username="root1";
+    static String password="12345A.";
+    static String dbUrl="jdbc:mysql://localhost:3306/sakila";
+
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(dbUrl, username,password);
+    }
+
+    public void showErrorMessage(SQLException exception){
+
+        System.out.println("Error :"+ exception.getMessage());
+        System.out.println("Error code : "+ exception.getErrorCode());
+    }
+
+}
